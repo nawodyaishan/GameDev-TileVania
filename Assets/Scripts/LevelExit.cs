cc26e3ad75aa62a986efd68680324c51da4a021f
nawodyaishan <nawodyain@gmail.com>
2022-06-30 01:52:52 +0530

    Add Level Asstes

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,9 +7,20 @@ namespace Assets.Scripts
 {
     public class LevelExit : MonoBehaviour
     {
+        [SerializeField] float levelLoadDelay;
+
+
+        // Coroutine for loading next level delay
         void OnTriggerEnter2D(Collider2D col)
         {
+            StartCoroutine(LoadNextLevel());
+        }
+
+        IEnumerator LoadNextLevel()
+        {
+            yield return new WaitForSecondsRealtime(levelLoadDelay);
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
         }
     }
 }
