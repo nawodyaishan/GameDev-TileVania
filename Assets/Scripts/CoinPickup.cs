ad9e97779524a2984b7c5e2f896a14e8ae4f8a65
nawodyaishan <nawodyain@gmail.com>
2022-06-30 16:43:39 +0530

    Add Score UI

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,18 +5,21 @@ namespace Assets.Scripts
 {
     public class CoinPickup : MonoBehaviour
     {
-        void Start()
-        {
-        }
+        [SerializeField] private AudioClip clip;
+        [SerializeField] private int coinPickup = 100;
 
-        void Update()
-        {
-        }
+        private bool wasCollected = false;
 
-        void OnCollisionEnter2D(Collision2D col)
+        void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.gameObject.CompareTag("Player"))
+            if (col.gameObject.CompareTag("Player") && !wasCollected)
+            {
+                wasCollected = true;
+                AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+                FindObjectOfType<GameSession>().AddToScore(coinPickup);
+                gameObject.SetActive(false);
                 Destroy(gameObject);
+            }
         }
     }
 }
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +8,13 @@ namespace Assets.Scripts
     public class GameSession : MonoBehaviour
     {
         [SerializeField] private int playerLives = 3;
+
+        [SerializeField] private TextMeshProUGUI livesText;
+
+        [SerializeField] TextMeshProUGUI scoreText;
+
+        private int gameScore = 0;
+
 
         void Awake()
         {
@@ -20,6 +28,12 @@ namespace Assets.Scripts
             {
                 DontDestroyOnLoad(gameObject);
             }
+        }
+
+        void Start()
+        {
+            livesText.text = playerLives.ToString();
+            scoreText.text = gameScore.ToString();
         }
 
         public void ProcessPlayerDeath()
@@ -38,6 +52,7 @@ namespace Assets.Scripts
         {
             SceneManager.LoadScene(0);
             Destroy(gameObject);
+            livesText.text = playerLives.ToString();
         }
 
         private void TakeLife()
@@ -45,6 +60,12 @@ namespace Assets.Scripts
             playerLives--;
             int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
             SceneManager.LoadScene(currentSceneIndex);
+        }
+
+        public void AddToScore(int pointsToAdd)
+        {
+            gameScore += pointsToAdd;
+            scoreText.text = gameScore.ToString();
         }
     }
 }
